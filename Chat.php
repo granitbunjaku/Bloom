2431eb50abbbc70f767ceb3a02acc0ddc5d5568d
= <granitbunjaku14@gmail.com>
2024-01-31 15:20:47 +0100

    versioni final

--- Chat.php
+++ Chat.php
@@ -28,7 +28,6 @@ class Chat implements MessageComponentInterface {
     }
 
     public function onClose(ConnectionInterface $conn) {
-        // The connection is closed, remove it, as we can no longer send it messages
         $this->clients->detach($conn);
 
         echo "Connection {$conn->resourceId} has disconnected\n";
